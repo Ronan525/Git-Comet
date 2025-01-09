9dfbe6a5ea80df2fb1539b49b292b0fa7815b1d0
Ronan Greenhalgh <RonanGreenhalgh@outlook.com>
2025-01-09 10:52:00 +0000

    Add secret key

--- git_comet/settings.py
+++ git_comet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@cb^+^%p&)&q&qsf0wzfs!j282$skb5^*a_8#hoj7z4et0q_%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
